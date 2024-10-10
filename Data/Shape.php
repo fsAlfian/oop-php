947b1cc5e5642412b718ebbfc6ee52e982d5288f
fsAlfian <alfian.horis@mcm.id>
2024-10-10 08:53:35 +0700

    add parent

---
+++ Data/Shape.php
@@ -0,0 +1,17 @@
+<?php
+
+class Shape {
+    function getCorner(): int{
+        return 0;
+    }
+}
+
+class Rectangle extends Shape {
+    public function getCorner(): int{
+        return 4;
+    }
+
+    public function getParentCorner(): int{
+        return parent::getCorner();
+    }
+}
